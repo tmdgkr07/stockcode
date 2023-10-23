b3237d78dd2f591d211f7d56f02a93b5794ed02b
tmdgkr07 <tmdgkr07@naver.com>
2023-10-23 17:16:22 +0900

    json저장위치변경

--- koreaconvert.py
+++ koreaconvert.py
@@ -29,5 +29,15 @@ def makejsonfile():
         os.remove(f"{name[i]}_code_part1.tmp")
         print(table_json)
         i=i+1
+    with open("./kospi.json", "w") as kospi_json:
+        data1 = json.load(kospi_json)
+    with open("./kosdaq.json", "w") as kosdaq_json:
+        data2 = json.load(kosdaq_json)      
+    with open("./remote/INFO/stock.json", "w") as new:
+        json.dump(data1+data2, new)
+    kospi_json.close()
+    kosdaq_json.close()
+    os.remove(kospi_json)
+    os.remove(kosdaq_json)
     return 0;
-# makejsonfile()
+ makejsonfile()
